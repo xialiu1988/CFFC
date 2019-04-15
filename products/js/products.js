e486a108af525dc6cfd2bc3288721da3feff4546
xialiu1988 <liuxia198896@gmail.com>
2019-04-15 11:19:52 -0700

    added productjs

---
+++ products/js/products.js
@@ -0,0 +1,14 @@
+'use strict';
+var products=[];
+function Product(name,src,price){
+  this.name=name;
+  this.src=src;
+  this.price=price;
+  products.push(this);
+}
+
+function render(){
+  
+}
+
+
