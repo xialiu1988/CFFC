95e4568f2f9a34361bc79ca9eb7c8a3855822d6c
williams osunkwo <wosunkwo@gmail.com>
2019-04-16 11:36:48 -0700

    completed the bar chart for the reports page

--- reports/js/reports.js
+++ reports/js/reports.js
@@ -1,18 +1,89 @@
-var membershipName = [];
-var membershipQuantity = [];
+'use strict';
+var membershipName = ['Gold', 'Silver', 'Bronze'];
+var membershipQuantity = [0,0,0];
 var productName = [];
-var prodcutQuantity = [];
+var productQuantity = [];
 
-getLocalStorageData(){
-  var membershipData = JSON.parse(localStorage.getItem('membershipData')) || [];
-  var productData = JSON.parse(localStorage.getItem('productData')) || [];
+function getLocalStorageData(){
+  var membershipData = JSON.parse(localStorage.getItem('membershipData'));
+  var productData = JSON.parse(localStorage.getItem('productData'));
   updateChartArrays(membershipData, productData);
 }
 
-updateChartArrays(membershipData, productData){
-  for(var i = 0; i< membershipData.length; i++){
-    membershipName.push(membershipData.productName);
-    membershipQuantity.push(membershipData.numberOfPurchases);
+function updateChartArrays(membershipData, productData){
+  for(var i=0; i<membershipData.length; i++){
+    membershipQuantity[membershipName.indexOf(membershipData[i].name)]++;
+    //console.log(membershipData[i].name + membershipName.indexOf(membershipData[i].name));
   }
-  
+  for(var j=0; j<productData.length; j++){
+    productName[j] = productData[j].name;
+    productQuantity[j] = productData[j].quantity;
+  }
+  drawMembershipChart();
+  drawProductChart();
 }
+function drawMembershipChart() {  
+  var canvas = document.getElementById('membership-chart');
+  var ctx = canvas.getContext('2d');
+
+  var data = {
+    labels: membershipName, // an array that stores the names of the membership types
+    datasets: [{
+      label: 'Total number of membership purchases',
+      data: membershipQuantity, // an array that stores the number of memberships that were purchased
+      backgroundColor: [
+        'goldenrod',
+        'silver',
+        '#cd7f32'
+      ],
+      hoverBackgroundColor: []
+    }]
+  };
+  var membershipChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1.0
+          }
+        }]
+      }
+    },
+  }); 
+}
+
+function drawProductChart(){
+  var canvas = document.getElementById('product-chart');
+  var ctx = canvas.getContext('2d');
+
+  var data = {
+    labels: productName, // an array that stores the names of the membership types
+    datasets: [{
+      label: 'Total number of product purchases',
+      data: productQuantity, // an array that stores the number of memberships that were purchased
+      backgroundColor: [
+        'blue','red','green','pink','brown','orange'
+      ],
+      hoverBackgroundColor: []
+    }]
+  };
+  var productChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1.0
+          }
+        }]
+      }
+    },
+  });
+}
+
+getLocalStorageData();
